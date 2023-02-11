1343c7f9137cffc6233ef3598eafb647ddd6a5f2
Azlan Ibrahim <azlibdar@gmail.com>
2023-02-11 20:12:14 +0530

    visual changes
    changes:
    - new input box design and added hover effects
    - button redesigned to look more attractive and clickable
    - changed background color of the results container - to look more
      natural
    - added the drop shadow on the flag
    - changed the data representation - now in tabular form
    - added some properties to look nice in mobile view

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 let btn = document.getElementById('btn');
 const resultdata = document.getElementById('ResultsData')
-document.addEventListener("keypress", function(event) {
-    if (event.key === 'Enter') {
-      document.getElementById("btn").click();
-    }
-  });
-  
+document.addEventListener("keypress", function (event) {
+  if (event.key === 'Enter') {
+    document.getElementById("btn").click();
+  }
+});
+
 btn.addEventListener('click', () => {
-    let inp = document.getElementById('inp').value;
+  let inp = document.getElementById('inp').value;
 
-    fetch(`https://restcountries.com/v3.1/name/${inp}?fullText=true`)
-        .then(response => response.json())
+  fetch(`https://restcountries.com/v3.1/name/${inp}?fullText=true`)
+    .then(response => response.json())
 
-        .then((data) => {
-            console.log(data[0])
-            resultdata.innerHTML = `
+    .then((data) => {
+      console.log(data[0])
+      resultdata.innerHTML = `
          <div id="img">
          <img src="${data[0].flags.png}">
          <h2>${data[0].altSpellings[1]}</h2>
@@ -24,20 +24,55 @@ btn.addEventListener('click', () => {
     </div>
     <div class="more">
     <div class="otherdata">
-    <p><b>Official Name </b>: ${data[0].altSpellings[2]}</p>
-    <p><b>Area:</b> ${data[0].area}</p>
-    <p><b>Capital:</b> ${data[0].capital}</p>
-    <p><b>Continent:</b> ${data[0].continents[0]}</p>
-    <p><b>Currency:</b> ${data[0].currencies[Object.keys(data[0].currencies)].name}</p>
-    <p><b>Languages :</b> ${Object.values(data[0].languages).toString().split(",").join(" , ")}</p>
-    <p><b>Maps:</b> <a href="${data[0].maps.googleMaps}">Check on Map</a></p>
-    <p><b>Population:</b> ${data[0].population}</p>
-    <p><b>Timezone :</b> ${data[0].timezones}</p>
-    <p><b>Sub Region:</b> ${data[0].subregion}</p>
-    <p><b>Un-Member:</b> ${data[0].unMember}</p>
+    <table>
+    <tr>
+    <th>Official name</th>
+    <td>${data[0].altSpellings[2]}</td>
+    </tr>
+    <tr>
+    <th>Area</th>
+    <td>${data[0].area}</td>
+    </tr>
+    <tr>
+    <th>Capital</th>
+    <td>${data[0].capital}</td>
+    </tr>
+    <tr>
+    <th>Continent</th>
+    <td>${data[0].continents[0]}</td>
+    </tr>
+    <tr>
+    <th>Currency</th>
+    <td>${data[0].currencies[Object.keys(data[0].currencies)].name}</td>
+    </tr>
+    <tr>
+    <th>Languages</th>
+    <td>${Object.values(data[0].languages).toString().split(",").join(" , ")}</td>
+    </tr>
+    <tr>
+    <th>Maps</th>
+    <td><a href="${data[0].maps.googleMaps}">Check on Map</a></td>
+    </tr>
+    <tr>
+    <th>Population</th>
+    <td>${data[0].population}</td>
+    </tr>
+    <tr>
+    <th>Timezone</th>
+    <td>${data[0].timezones}</td>
+    </tr>
+    <tr>
+    <th>Sub Region</th>
+    <td>${data[0].subregion}</td>
+    </tr>
+    <tr>
+    <th>Un-Member</th>
+    <td>${data[0].unMember}</td>
+    </tr>
+    </table>
     </div>
     </div> `
-})
+    })
 
 
 })  
